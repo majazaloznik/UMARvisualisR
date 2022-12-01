406f1a4e160f6b25436a118c65863144e4f1c5d9
majazaloznik <maja.zaloznik@gmail.com>
2022-12-01 17:04:41 +0100

    Fixed multi fun export :bug:

--- R/misc.R
+++ R/misc.R
@@ -2,7 +2,8 @@
 #'
 #' in_range checks if the value is within the range, including the limits,
 #' in_range_strict excludes the limits.
-#'
+
+#' @name ranges
 #' @param x numeric vector of length one
 #' @param r a range - numeric vector of lenght two, ascending
 #'
@@ -10,15 +11,12 @@
 #'
 #' @rdname ranges
 #' @export
-#'
-
 in_range <- function (x, r) {
   if(!(length(r) == 2 && r[1] <= r[2])) stop("not a real range")
   return(x >= r[1] & x <= r[2])
 }
 
-#' @rdname ranges
-#' @export
+
 in_range_strict <- function (x, r) {
   if(!(length(r) == 2 && r[1] <= r[2])) stop("not a real range")
   return(x > r[1] & x < r[2])
