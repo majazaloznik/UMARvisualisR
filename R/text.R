4ab5ede6343e5620b9375ea226cb63678a66304d
majazaloznik <maja.zaloznik@gmail.com>
2022-12-05 10:00:15 +0100

    Add max_rows parameter to wrap_text & test

--- R/text.R
+++ R/text.R
@@ -4,18 +4,22 @@
 #' Uses strwrap to split a string into subsets that are shorter than the
 #' specified width and inserts newlines in between them. Inspired by this
 #' [SO answer](https://stackoverflow.com/questions/7367138/text-wrap-for-plot-titles/7367534#7367534).
+#' Also returns the number of rows it has produced, and an optional max_row parameter
+#' lets you set a threshold for the number of rows after which the text gets truncated.
 #'
 #' @param string a character string to be wrapped- vector of length one
-#' @param width numeric - desired width
+#' @param width numeric - desired width, defaults to 100
+#' @param max_rows numeric maximum number of rows allowed in the output, defaults to 3
 #'
-#' @return a list with the first element a character string with new lines if required and the
+#' @return a list with the first element a character string with new lines if required, and the
 #' second one a numeric value for how many rows are in the returned string.
 #'
 #' @export
 #'
-wrap_string <- function(string, width){
+wrap_string <- function(string, width = 100, max_rows = 3){
   split_text <- strwrap(string,
                         width = width)
+  split_text <- split_text[1:min(length(split_text), max_rows)]
   n <- length(split_text)
   wrapped_text <- paste(split_text,
         collapse="\n")
--- tests/testthat/test-text.R
+++ tests/testthat/test-text.R
@@ -3,4 +3,7 @@ input <- "testing 123 testing 234"
   out <- wrap_string(input, 12)
   expect_equal(nchar(input) + 1, nchar(encodeString(out[[1]])))
   expect_equal(out[[2]], 2)
+  input <- "12 34 56 78"
+  out <- wrap_string(input, 2)
+  expect_equal(out[[2]], 3)
 })
