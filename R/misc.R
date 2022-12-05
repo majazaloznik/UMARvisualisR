98861024390d8d174c12ccef983d997ad55cd0d9
majazaloznik <maja.zaloznik@gmail.com>
2022-12-05 15:47:00 +0100

    Pretty y-limit helper fun wiht test

--- R/misc.R
+++ R/misc.R
@@ -38,3 +38,21 @@ first_up <- function(x) {
   stringr::str_replace(x, "^\\w{1}", toupper)
 }
 
+#' Find pretty y-axis limits
+#'
+#' Finds ylimits that lead to a pretty y-axis. If the range of the values is
+#' on the line, it artificially extends it by one more break.
+#'
+#' @param values vector of numeric values
+#'
+#' @return numeric vector of length 2 to be used as y-axis limit
+#' @export
+#'
+find_pretty_ylim <- function(values){
+  ylim <- range(pretty(c(values)), na.rm = TRUE)
+  if(ylim[1] == min(values, na.rm = TRUE)) {
+    values <- c(values, min(values, na.rm = TRUE)*0.99)}
+  if(ylim[2] == max(values, na.rm = TRUE)) {
+    values <- c(values, max(values, na.rm = TRUE)*1.01)}
+  range(pretty(c(values)), na.rm = TRUE)
+}
