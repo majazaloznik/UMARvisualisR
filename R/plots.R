0a7a46843eaf750e54d41cebeea385b6e12bca22
majazaloznik <maja.zaloznik@gmail.com>
2022-12-06 16:57:10 +0100

    Add error chart if all data is missing

--- R/plots.R
+++ R/plots.R
@@ -23,12 +23,90 @@ univariate_line_chart <- function(prep_l){
   sub_title <- prep_l[[4]]
   title_lines <-main_title[[2]] + sub_title[[2]]
 
-  # prepare plot area and limits
-  par(mar = c(3, 3.5, title_lines + 1, 1))
-  ylim <- find_pretty_ylim(single$value)
+  if(all(is.na(single$value))) {
+    na_chart(prep_l) } else {
+      # prepare plot area and limits
+      par(mar = c(3, 3.5, title_lines + 1, 1))
+      ylim <- find_pretty_ylim(single$value)
+
+      #plot
+      plot(single$period, single$value, type = "l",
+           col = umar_cols("rdeca"),
+           lwd = 2,
+           xlab = "", ylab = "",
+           bty = "n",
+           axes = FALSE,
+           main = "",
+           cex.main = 1,
+           family ="Myriad Pro",
+           panel.first={grid(nx = NA, ny = NULL, col = umar_cols("gridlines"), lty = 1)},
+           yaxs="i",
+           ylim = ylim)
+
+      # axis tickmarks
+      axis.Date(1,at=seq(min(single$period), max(single$period), by="1 year"),
+                col = umar_cols("gridlines"),
+                lwd = 0, lwd.ticks =2, tck=-0.01, labels = FALSE)
+
+      # shifting year labels by six months
+      ss <- as.POSIXlt(min(single$period))
+      ss$mon <- as.POSIXlt(min(single$period))$mon+6
+      ee <- as.POSIXlt(max(single$period))
+      ee$mon <- as.POSIXlt(max(single$period))$mon+6
+
+      axis.Date(1,at=seq(ss, ee, by="1 year"),
+                col = umar_cols("gridlines"),
+                lwd = 0, tck = 0,  family ="Myriad Pro",
+                las = 2, padj = 0.5)
+
+      axis(2, col = umar_cols("gridlines"),lwd = 0,  tck=0.0,
+           las = 2,  family ="Myriad Pro")
+      box(col = umar_cols("gridlines"), lwd = 2)
+
+      # titles and labels
+      mtext(main_title[[1]], side = 3, line = 0.5 + sub_title[[2]],
+            family ="Myriad Pro", font = 2)
+      mtext(sub_title[[1]], side = 3,
+            line = 0.5, family ="Myriad Pro")
+      mtext(unit, side = 2,
+            line = 2.5, family ="Myriad Pro")
+
+      # emphasised gridline
+      if(unit %in% c("Indeks", "Index", "indeks", "index", "%")) {
+        abline(h = 100, col = umar_cols("emph"), lwd = 1.5)}
+      if(in_range_strict(0, ylim))  {
+        abline(h = 0, col = umar_cols("emph"), lwd = 1.5)}
+    }
+}
+
+
+
+#' Plotting function for chart with no data values
+#'
+#' Plots error chart with message when all the data values in the chart are
+#' missing. To be used as a temporary placeholder in reports where the selection
+#' accidentally doens't include any data. Input data must be prepared by
+#' \link[UMARaccessR]{prep_single_line}. Called from  \link[UMARvisualisR]{univariate_line_chart}
+#' if all values are NA.
+#'
+#' @param prep_l list of length four with data.frame with data, the unit used and the
+#' main and sub titles. see \link[UMARaccessR]{prep_single_line}.
+#'
+#' @return nothing, plots to open device
+#' @export
+#'
+
+na_chart <- function(prep_l){
+  single <- prep_l[[1]]
+  unit <- prep_l[[2]]
+  main_title <- prep_l[[3]]
+  sub_title <- prep_l[[4]]
+  title_lines <-main_title[[2]] + sub_title[[2]]
+
+  ylim <- c(0,1)
 
   #plot
-  plot(single$period, single$value, type = "l",
+  plot(single$period, rep(0.5, length(single$value)), type = "l",
        col = umar_cols("rdeca"),
        lwd = 2,
        xlab = "", ylab = "",
@@ -57,8 +135,6 @@ univariate_line_chart <- function(prep_l){
             lwd = 0, tck = 0,  family ="Myriad Pro",
             las = 2, padj = 0.5)
 
-  axis(2, col = umar_cols("gridlines"),lwd = 0,  tck=0.0,
-       las = 2,  family ="Myriad Pro")
   box(col = umar_cols("gridlines"), lwd = 2)
 
   # titles and labels
@@ -66,13 +142,10 @@ univariate_line_chart <- function(prep_l){
         family ="Myriad Pro", font = 2)
   mtext(sub_title[[1]], side = 3,
         line = 0.5, family ="Myriad Pro")
-  mtext(unit, side = 2,
-        line = 2.5, family ="Myriad Pro")
-
-  # emphasised gridline
-  if(unit %in% c("Indeks", "Index", "indeks", "index", "%")) {
-    abline(h = 100, col = umar_cols("emph"), lwd = 1.5)}
-  if(in_range_strict(0, ylim))  {
-    abline(h = 0, col = umar_cols("emph"), lwd = 1.5)}
+  mtext("Podatki ne obstajajo", side = 3, cex=2, font = 3,
+        line = -5.5, family ="Myriad Pro", col = umar_cols("rdeca"))
+  mtext("(cela serija je za izbrano obdobje prazna)", side = 3, cex=1, font = 3,
+        line = -6.5, family ="Myriad Pro", col = umar_cols("rdeca"))
 }
+
 
--- tests/testthat/test-plots.R
+++ tests/testthat/test-plots.R
@@ -3,4 +3,9 @@ prep_l <- readRDS(test_path("testdata", "prep_l.rds"))
 test_that("multiplication works", {
   p <- function() univariate_line_chart(prep_l)
   vdiffr::expect_doppelganger("univariate line", p)
+  prep_l[[1]] <- prep_l[[1]][1:100,]
+  p <- function() univariate_line_chart(prep_l)
+  vdiffr::expect_doppelganger("na chart", p)
+  p <- function() na_chart(prep_l)
+  vdiffr::expect_doppelganger("na chart2", p)
 })
