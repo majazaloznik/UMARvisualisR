1ede19994b83bf51c4d3426606f66cab3515bd28
majazaloznik <maja.zaloznik@gmail.com>
2022-12-14 15:14:00 +0100

    Add transf alignment txt

--- R/transformations.R
+++ R/transformations.R
@@ -18,6 +18,10 @@ rolling_average <- function(df, periods = 3, align = "center"){
 #' @export
 add_rolling_average <- function(prep_l, periods = 3, align = "center") {
   prep_l[[1]] <- rolling_average(prep_l[[1]], periods = periods, align = align)
-  prep_l <- c(prep_l, transf_txt = paste0("Transf.: ", periods,"-",prep_l[["interval"]], " drse\u010da sredina"))
+  align_txt <- ifelse(align == "center", " (centrirana)",
+                      ifelse(align == "right", " (desna)",
+                             ifelse(align == "left", " (leva)", "")))
+  prep_l <- c(prep_l, transf_txt = paste0("Transf.: ", periods,"-",prep_l[["interval"]],
+                                          " drse\u010da sredina", align_txt))
   prep_l
 }
