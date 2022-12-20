febbd40fd26edf2ea23543f84a3486a1a01deadb
majazaloznik <maja.zaloznik@gmail.com>
2022-12-20 13:33:39 +0100

    Finish refactoring prep_single_line

--- R/umbrellas.R
+++ R/umbrellas.R
@@ -11,6 +11,10 @@
 #' @param roll_periods number of periods to roll over (defualt = 3)
 #' @param roll_align alignment of rolling average (default = "center")
 #' @param yoy logical - should the year on year change be calculated and displayed
+#' @param interval character describing interval type
+#' @param unit character describing unit to be written as y label
+#' @param main_title character..
+#' @param sub_title character...
 #' @inheritParams common_parameters
 #'
 #' @return plots a univariate line chart
@@ -21,9 +25,14 @@ univariate_line_pipeline <- function(series,
                              xmin = "2011-01-01", xmax =NULL,
                              rolling = FALSE, roll_periods = 3, roll_align = "center",
                              yoy = FALSE,
+                             interval = NULL,
+                             unit = NULL,
+                             main_title = NULL,
+                             sub_title = NULL,
                              con) {
   vintage_id <- UMARaccessR::get_vintage_from_series(series, con, date_valid = date_valid)
-  prep_l <- prep_single_line(vintage_id, con)
+  prep_l <- prep_single_line(vintage_id, con, interval = interval, unit = unit,
+                             main_title = main_title, sub_title = sub_title)
   if(rolling & yoy){
     prep_l <- add_yoy_of_rolling(prep_l, periods = roll_periods, align = roll_align)} else {
       if(rolling) prep_l <- add_rolling_average(prep_l, periods = roll_periods, align = roll_align)
