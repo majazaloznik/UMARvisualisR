e7c0a536352a62286283ce05d026da4cc66256d4
majazaloznik <maja.zaloznik@gmail.com>
2023-01-03 13:21:11 +0100

    ffs

--- tests/testthat/sandbox/select-6d9ed5.R
+++ tests/testthat/sandbox/select-6d9ed5.R
@@ -1,2 +1,2 @@
-structure(list(name = "Indeksi nominalnega prihodka v storitvenih dejavnostih (mesec / povpre?je leta 2015) po skupinah dejavnosti (SKD 2008), originalni podatki in podatki, iz katerih so izlo?eni vplivi sezone in koledarja, Slovenija, mese?no"), class = "data.frame", row.names = c(NA, 
+structure(list(name = "Indeksi nominalnega prihodka v storitvenih dejavnostih (mesec / povprečje leta 2015) po skupinah dejavnosti (SKD 2008), originalni podatki in podatki, iz katerih so izločeni vplivi sezone in koledarja, Slovenija, mesečno"), class = "data.frame", row.names = c(NA,
 -1L))
