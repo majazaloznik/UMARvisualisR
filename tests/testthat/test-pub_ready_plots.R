1b5b2f15c39281c2fa3e1bf099716118f314ac89
majazaloznik <maja.zaloznik@gmail.com>
2023-11-15 10:57:16 +0100

    test fix

--- tests/testthat/test-pub_ready_plots.R
+++ tests/testthat/test-pub_ready_plots.R
@@ -25,7 +25,7 @@ dittodb::with_mock_db({
     config <- list(series = c(1,2,3),
                    legend_columns = 2,
                    title = "Very very long title that just keeps going on and on and hopefully takes up at least two lines for me to be able to test this shit properly.")
-    top <- get_top_margin_and_title(config, 1, 10)
+    top <- get_top_margin_and_title(config, 10)
     expect_true(top[[1]]> 3.1 & top[[1]] < 3.2)
     expect_equal(top[[2]], 1.46)
 
