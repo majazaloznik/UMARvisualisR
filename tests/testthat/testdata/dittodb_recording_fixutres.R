4d5b5f68f46fd9c0ea770b09edb2115a43e9646a
majazaloznik <maja.zaloznik@gmail.com>
2023-11-30 13:43:39 +0100

    Clean up old dygraphing stuff

--- tests/testthat/testdata/dittodb_recording_fixutres.R
+++ tests/testthat/testdata/dittodb_recording_fixutres.R
@@ -14,40 +14,8 @@ library(dittodb)
 # out <- prep_single_line(1625, con)
 # stop_db_capturing()
 #
-# start_db_capturing()
-# con <- dbConnect(RPostgres::Postgres(),
-#                  dbname = "sandbox",
-#                  host = "localhost",
-#                  port = 5432,
-#                  user = "mzaloznik",
-#                  password = Sys.getenv("PG_local_MAJA_PSW"))
-# dbExecute(con, "set search_path to test_platform")
-# on.exit(dbDisconnect)
-# out <- univariate_line_pipeline(1625, con=con)
-# stop_db_capturing()
-#
-#
-#
-# start_db_capturing()
-# con <- dbConnect(RPostgres::Postgres(),
-#                  dbname = "sandbox",
-#                  host = "localhost",
-#                  port = 5432,
-#                  user = "mzaloznik",
-#                  password = Sys.getenv("PG_local_MAJA_PSW"))
-# dbExecute(con, "set search_path to test_platform")
-# on.exit(dbDisconnect)
-# univariate_line_pipeline(1234,
-#                          date_valid = NULL,
-#                          xmin = "2011-01-01", xmax =NULL,
-#                          rolling = FALSE, roll_periods = 3, roll_align = "center",
-#                          yoy = FALSE,
-#                          interval = NULL,
-#                          unit = NULL,
-#                          main_title = NULL,
-#                          sub_title = NULL,
-#                          con = con)
-# stop_db_capturing()
+
+
 #
 #
 # start_db_capturing()
