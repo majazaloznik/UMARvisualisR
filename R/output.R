4c38d09bce5fb5a09aacfcdc5b0e51ea8727f58b
majazaloznik <maja.zaloznik@gmail.com>
2023-12-05 17:28:36 +0100

    umbrella funs for outputting to pdf dev

--- R/output.R
+++ R/output.R
@@ -19,7 +19,39 @@ pdf_output <- function(filename, size = "normal") {
         width <-  22*cm
         height <- 8*cm}
 
-  grDevices::cairo_pdf(filename, width, height)
+  new_filename <- chec_and_rename_pdf(filename)
+
+  grDevices::cairo_pdf(new_filename, width, height)
 
 }
 
+
+#' Checks if pdf is open/locked and renames filename
+#'
+#' if the pdf you are trying to write to is open, it is also locked for
+#' writing. so in this case we rename the filename by adding the timestamp
+#' to the end of the filename. kludgy but works.
+#'
+#' @param filename file to check
+#'
+#' @return (updated) filename
+#' @export
+#'
+chec_and_rename_pdf <- function(filename) {
+  if (file.exists(filename)) {
+    tryCatch({
+      con <- file(filename, open = "r+")
+      close(con)
+      filename
+    }, warning = function(w) {
+      timestamp <- format(Sys.time(), "%Y%m%d%H%M%S")
+      new_filename <- sub("\\.pdf$", paste0("_", timestamp, ".pdf"), filename)
+      message("File is locked, saving to a new file: ", new_filename)
+      new_filename
+    }, error = function(e) {
+      stop("Error in accessing file: ", e$message)
+    })
+  } else {
+    filename
+  }
+}
--- R/umbrellas.R
+++ R/umbrellas.R
@@ -89,3 +89,45 @@ multiline_pipeline  <- function(df, con, xmin = "2011-01-01", xmax =NULL,
     write_to_sheet(prep_l, df, wb=wb, odd)
   }
 }
+
+
+#' Full pipeline for publicaiton ready charts
+#'
+#' Input appropriate excel file with config info and produces an english and
+#' slovenian version of the chart in pdf format, currently to the hardcoded
+#' folder "O:\\Avtomatizacija\\umar-publication-ready-grafi\\grafi\\",
+#' but this can of course be changed if needed.
+#'
+#' @param input_file excel with single sheet and all relevant info for config.
+#' see vingette (non existent as of now) for full details.
+#'
+#' @return nothing, produces pdf charts
+#' @export
+publication_ready_plot_pipeline <- function(input_file){
+  # setup
+  output_path <-"O:\\Avtomatizacija\\umar-publication-ready-grafi\\grafi\\"
+  filename <- basename(input_file)
+  base_filename <- sub('\\.xlsx$', '', filename)
+  # get and prep data
+  x <- openxlsx::read.xlsx(input_file)
+  results <- prep_data(x, con)
+  out <- split_by_unit(results$datapoints, results$config)
+  datapoints <- out$datapoints
+  config <- out$config
+
+  size <- ifelse(config$chart_size == 1, "small",
+                 ifelse(config$chart_size == 2, "normal", "large"))
+  # output file names
+  output_si <- paste0(output_path, base_filename, "_si", ".pdf")
+  output_en <- paste0(output_path, base_filename, "_en", ".pdf")
+
+  # output slovenian chart
+  pdf_output(output_si, size)
+  publication_ready_plot(datapoints, config)
+  dev.off()
+
+  #output english chart
+  pdf_output(output_en, size)
+  publication_ready_plot(datapoints, config, "en")
+  dev.off()
+}
